7a47e741c3feed0a22c069f78a7a827b627868ea
mavaec <mavaec@gmail.com>
2023-04-23 20:48:04 +0200

    Final

--- budget.py
+++ budget.py
@@ -1,21 +1,11 @@
 class Category :
 
-	#budget_cat = ('food', 'clothing', 'entertainment')
-	# self.budget_cat = None
-	# self.ledger = []
-	# self.budget_tot = 0
-
 	def __init__(self, type_cat) :
-		#print('Object created')
-		# if type in self.budget_cat :
-		# 	print('It is')
-		# else :
-		# 	print('NOT a valid category')
 		self.budget_cat = type_cat
 		self.ledger = []
 		self.budget_tot = 0
 
-		print('Created objet',  self.budget_cat)
+		#print('Created objet',  self.budget_cat)
 
 	def deposit(self, amnt, desc) :
 		self.ledger.append((amnt, desc))
@@ -33,14 +23,6 @@ class Category :
 
 
 	def get_balance(self) :
-		# print(self.budget_cat.center(30, '*'))
-		# for (amnt,desc) in self.ledger :
-		# 	amnt_decimals = "{0:.2f}".format(amnt)
-		# 	len_amnt = len(amnt_decimals)
-		# 	len_desc = len(desc)
-		# 	print(desc[0:23].ljust(23,' ') + str(amnt_decimals)[-7:].rjust(7, ' '))
-		# print('Total:', "{0:.2f}".format(self.budget_tot))
-
 		balance = ''
 		balance = balance + self.budget_cat.center(30, '*') + '\n'
 		for (amnt,desc) in self.ledger :
@@ -70,19 +52,54 @@ class Category :
 		else :
 			return False
 
+#-------------------------------------------------------------------------------
 def create_spend_chart(list_cat) :
-	expenditure = 'Percentage spent by category\n'
-	tot_exp = 0
+	exp_tot = 0
+	exp_dict = {}
 	chart_dict = {}
+	max_length = 0
 	for bCat in list_cat :
 		withdrawal = 0
 		for (amnt,desc) in bCat.ledger :
 			if amnt < 0 and 'Transfer' not in desc :
 				withdrawal += abs(amnt)
-				tot_exp += abs(amnt)
-		chart_dict[bCat.budget_cat] = withdrawal
+				exp_tot += abs(amnt)
+		exp_dict[bCat.budget_cat] = withdrawal
+		if len(bCat.budget_cat) > max_length :
+			max_length = len(bCat.budget_cat)
+	
+	for bCat, exp in exp_dict.items() :
+		chart_dict[bCat] = round(100 * exp/exp_tot)
 
-		print(chart_dict)
+	expenditure = 'Percentage spent by category\n'
+	percentage = reversed(range(0, 110, 10))
+	for n in percentage :
+		ratio_to_show = ''
+		horiz_line = '    ' 
+		for v in chart_dict.values() :
+			if v >= n :
+				ratio_to_show += ' o '
+			elif (n - v) <= 5 :
+				ratio_to_show += ' o '
+			else :
+				ratio_to_show += '   '
+			horiz_line += '---' 
+
+		expenditure = expenditure + str(n).rjust(3) + '|' + ratio_to_show + '\n'
+	horiz_line += '-' 
+	expenditure += horiz_line + '\n'
+
+
+	cat_name = ''
+	for char in range(0, max_length) :
+		cat_name += '    '
+		for cat in chart_dict.keys() :
+			if char < len(cat) :
+				cat_name += cat[char].center(3)
+			else :
+				cat_name += '   '
+		cat_name += '\n'
+	expenditure += cat_name
 
 
 	return expenditure
--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from budget import Category, create_spend_chart
 
 bFood = Category('food')
 bClothing = Category('clothing')
+bEnter = Category('entertaintment')
 
 
-
-bFood.deposit(1200.0, 'Initial deposit of 0abcdef')
+bFood.deposit(500.0, 'Initial deposit of 0af')
 bFood.deposit(30.259999, 'Refund')
-bFood.withdraw(10, 'Wine')
+bFood.withdraw(10.00, 'Wine')
 bFood.withdraw(50, 'Groceries')
 
-bFood.transfer(500, bClothing)
-bClothing.withdraw(10, 'Pants')
+bFood.transfer(100, bClothing)
+bClothing.withdraw(60, 'Pants')
+bClothing.withdraw(20, 'Socks')
+
+bEnter.deposit(250, 'Initial deposit')
+bEnter.withdraw(85, 'Theater')
 
 print(bFood.get_balance())
 print(bClothing.get_balance())
-#print(bFood.check_funds(10))
+print(bEnter.get_balance())
 
-print(create_spend_chart([bFood, bClothing]))
+print(create_spend_chart([bFood, bClothing, bEnter]))
